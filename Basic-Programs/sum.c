220fbf163afb7a5aad22c98d77b880ea2cc3cdd0
VijayKolekar137 <76657479+VijayKolekar137@users.noreply.github.com>
2021-04-28 17:55:57 +0530

    Add files via upload

---
+++ Basic-Programs/sum.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+int main()
+{
+    /* Addition of two number (User input) */
+
+    int a,b,c;
+
+    printf("Enter the first number= ");
+
+    scanf("%d",&a);
+
+    printf("\n");
+
+    printf("Enter the second number= ");
+
+    scanf("%d",&b);
+
+    printf("\n");
+
+    c=a+b;
+
+    printf("Addition of two number= %d",c);
+
+    return 0;
+}
+
