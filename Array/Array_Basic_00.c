881ed7cb74e4b02f888f1f56f215f7bef71f6c51
VijayKolekar137 <76657479+VijayKolekar137@users.noreply.github.com>
2021-07-05 21:48:23 +0530

    Add files via upload

---
+++ Array/Array_Basic_00.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main()
+{
+    int Marks[4];
+
+    Marks[0]= 50;
+    Marks[1]= 66;
+    Marks[2]= 88;
+    Marks[3]= 77;
+
+    printf("The value at Marks[0] is %d\n", Marks[0]);
+    printf("The value at Marks[1] is %d\n", Marks[1]);
+    printf("The value at Marks[2] is %d\n", Marks[2]);
+    printf("The value at Marks[3] is %d\n", Marks[3]);
+
+    return 0;
+}
